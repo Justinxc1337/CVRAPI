518e78018b11c41b420095d40819491a7fd8e361
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-10-26 15:24:33 +0200

    API kode

--- api.js
+++ api.js
@@ -1,13 +1,27 @@
+function getCompanyInfo() {
+  const cvrNumber = document.getElementById('cvrNumber').value;
 
+  if (!cvrNumber) {
+      alert('Indtast venligst et CVR-nummer.');
+      return;
+  }
 
-/* 61056416 */
-
-async function virk(int) {
-    const requestURL = `https://cvrapi.dk/api?search=${int}&country=dk`;
-  const request = new Request(requestURL);
-
-  const response = await fetch(request);
-  const virk = await response.json();
+  // Foretag et API-opkald til cvrapi.dk
+  fetch(`https://cvrapi.dk/api?search=${cvrNumber}&country=dk`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data && data.name) {
+          const companyInfo = document.getElementById('companyInfo');
+          companyInfo.innerHTML = `
+              <p>Virksomhedens navn: ${data.name}</p>
+              <p>CVR: ${data.vat}</p>
+              <p>Adresse: ${data.address}</p>
+          `;
+      } else {
+          document.getElementById('companyInfo').textContent = 'Ingen resultater fundet.';
+      }
+  })
+  .catch((error) => {
+      console.error('Fejl ved hentning af data:', error);
+  });
 }
-
-virksomhedsdata(virk, 20);
